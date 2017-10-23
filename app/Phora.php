d4ae60cbe8ebd7e069f440e1d821f084caa9d9c3
LORD KAY <offeilord@gmail.com>
2017-10-23 22:21:58 +0000

    Completed the wordwrapper function

--- app/Phora.php
+++ app/Phora.php
@@ -15,8 +15,34 @@ class Phora
 	* params data eg.banks , length eg.10 , delimiter eg. "...."
 	*/
 
-	public static function WordWrap(string $name = NULL, int $length = 0, string $delimiter = "..."){
-		
+	public static function WordWrap(string $name = NULL, int $length = 0, string $limiter = "..."){
+		$word = $name;
+		$getLength = strlen($name);
+		$wordSize  = $length;
+		$delimiter = $limiter;
+
+		if (!empty($word) && is_string($word)) {
+			if ($getLength > 0 && $getLength >= $wordSize) {
+				$wordwrap =  "";
+
+				//Appending and combining
+				for ($i=0; $i <= $wordSize ; $i++) { 
+					if ($i == $wordSize) {
+						$wordwrap.= " ".$delimiter;
+					}
+					else{
+						$wordwrap .= $word[$i];
+					}
+				}
+				return $wordwrap;
+			}
+			else{
+				return $word;
+			}
+		}else{
+			return "First Parameter is either empty or not a string. Provide Correct Parameters";
+		}
 	}
+
 }
  ?>
