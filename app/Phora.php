68e2cbddf91db87d4a0d71745c9ddfc0b3d249f1
LORD KAY <offeilord@gmail.com>
2017-10-23 22:08:50 +0000

    Started with the initiation of the word wrapper

---
+++ app/Phora.php
@@ -0,0 +1,22 @@
+<?php 
+/**
+* @Functions and methods are defined here
+* @var params
+* author : LORD KAY
+* email  : offeilord@gmail.com
+* github : github.com/LORD-KAY
+*/
+class Phora
+{
+	/**
+	* Truncation function / Word Wrapper
+	* @var params
+	* Static Function
+	* params data eg.banks , length eg.10 , delimiter eg. "...."
+	*/
+
+	public static function WordWrap(string $name = NULL, int $length = 0, string $delimiter = "..."){
+		
+	}
+}
+ ?>
