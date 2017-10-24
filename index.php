635516243617d783021ba2298a7b067bc8d53296
LORD KAY <offeilord@gmail.com>
2017-10-24 08:07:31 +0000

    This is the tester

--- index.php
+++ index.php
@@ -3,8 +3,9 @@ ini_set( 'error_reporting', E_ALL );
 ini_set( 'display_errors', true );
 include 'app/Phora.php';
 $sample = new Phora();
-$string_name = "acheampong lord Offei Banks";
 
-$results = $sample::WordCap($string_name);
- echo "$results";
+$data = "acheampong lord, offei";
+$results = $sample->WordWrap($data,14);
+var_dump($results);
+
  ?>
